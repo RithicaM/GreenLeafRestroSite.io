864d2f576f08fad1b299abebff52d76761003b22
RITHICA M <86734162+RithicaM@users.noreply.github.com>
2021-07-08 22:27:08 +0530

    Delete script1.js

--- script1.js
+++
@@ -1,7 +0,0 @@
-+const readMoreBtn = document.querySelector(".read-more-btn");
-const text = document.querySelector(".text");
-
-readMoreBtn.addEventListener("click", (e) => {
-  text.classList.toggle("show-more");
-});
-
